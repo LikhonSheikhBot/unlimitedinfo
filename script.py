92946fa45cfba3e664b5ed96621ae53b9ee7210b
Likhon Sheikh <93394527+LikhonSheikhBot@users.noreply.github.com>
2021-11-10 03:18:35 +0600

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @GodFatherMob</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Likhon Sheikh</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @GodFatherMob    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
